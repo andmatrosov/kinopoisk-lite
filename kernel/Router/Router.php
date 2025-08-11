d688bce3840347de21b285446caf2c0d83e527a4
andmatrosov <and.matrosow@ya.ru>
2025-08-11 04:06:03 +0400

    first commit

---
+++ kernel/Router/Router.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Kernel\Router;
+
+class Router
+{
+    private array $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
+
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
+
+    public function dispatch(string $uri, string $method): void
+    {
+        $route = $this->findRoute($uri, $method);
+
+        if (! $route) {
+            $this->notFound();
+        }
+
+        if (is_array($route->getAction())) {
+            [$controller, $action] = $route->getAction();
+
+            $controller = new $controller;
+
+            call_user_func([$controller, $action]);
+        } else {
+            call_user_func($route->getAction());
+        }
+
+    }
+
+    private function notFound(): void
+    {
+        echo '404 Not Found';
+        exit;
+    }
+
+    private function initRoutes(): void
+    {
+        $routes = $this->getRoutes();
+
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUri()] = $route;
+        }
+    }
+
+    private function findRoute($uri, $method): Route|false
+    {
+        if (! isset($this->routes[$method][$uri])) {
+            return false;
+        }
+
+        return $this->routes[$method][$uri];
+    }
+
+    /**
+     * Get the routes from the configuration file.
+     *
+     * @return Route[]
+     */
+    private function getRoutes()
+    {
+        return require_once APP_PATH.'/config/routes.php';
+    }
+}
